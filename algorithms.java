697d3a88781e6e511fc5b02b3ef6a28ec34caf0e
Youssef Kholeif <yykholeif@gmail.com>
2017-12-02 21:36:46 +0000

    more Java algorithms

--- algorithms.java
+++ algorithms.java
@@ -73,10 +73,92 @@ public void printInReverse(String[] stringArray) {
 }
 
 
+//print range
+
+public int findRange(int[] intArray) {
+	//find min and max
+	if (intArray.length == 0) {
+		return -1;	
+	}
+	double min = intArray[0];
+	double max = intArray[0];
+	for(int i=1 ; i < intArray.length ; i++) {
+		if(intArray[i] < min) {
+			min = intArray[i];
+		}
+		if(intArray[i] > max) {
+			max = intArray[i];
+		}
+	}
+	return max - min;
+}
 
 
 
+/* print out using double loop
+#
+##
+### 
+*/
 
 
+int rows = 3;
+for (int i=1; i<=rows; i++) {
+	String thisRow = "";
+	for (int j=0; j<i; j++) {
+		thisRow = thisRow + "#";
+	}
+	System.out.println(thisRow);
+}
+
+//monopoly roll original
+
+public int randomInt() {
+	double number = Math.random() * 6;
+	number = number + 1;
+	return (int) number;
+}
+
+public int monopolyRoll() {
+	int firstRole = randomInt();
+	int secondRole = randomInt();
+	int sum = firstRole + secondRole;
+
+	if (firstRole == secondRole) {
+		int thirdRole = randomInt();
+		int fourthRole = randomInt();
+		sum = sum + thirdRole + fourthRole
+	}
+	return sum
+}
+
+//monopoly roll using loop
+
+public int randomInt(int sides) {
+	double number = Math.random() * sides;
+	number = number + 1;
+	return (int) number;
+}
+
+public int monopolyRoll() {
+	int firstRoll = randomInt(6);
+	int secondRoll = randomInt(6);
+	int total = firstRole + secondRole;
+
+	rollCount = 1;
+	while (firstRoll == secondRoll) {
+
+		if(rollCount >=3) return -1;
+
+		firstRoll = randomInt(6);
+		secondRoll = randomInt(6);
+		total = total + firstRoll + secondRoll;
+
+		rollCount += 1
+	}
+	return total;
+}
+
+//monopoly roll recursive
 
 
