f0e1cfde66e39a6ba02c092cbdf00c4e5687c523
Youssef Kholeif <yykholeif@gmail.com>
2018-01-02 22:00:17 +0200

    Create spaceship interface for rocket class and extend rocket class to missile types

--- ooj/MissionToMars/src/Item.java
+++ ooj/MissionToMars/src/Item.java
@@ -1,12 +1,15 @@
 public class Item {
     private String name;
-    private int weight;
+    public int weight;
 
-     public Item(String n, int w) {
-        this.name = n;
-        this.weight = w;
+     public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
     }
 
     //getter method
+    public int weight() {
+         return this.weight;
+    }
 
 }
--- ooj/MissionToMars/src/Rocket.java
+++ ooj/MissionToMars/src/Rocket.java
@@ -4,34 +4,35 @@ public class Rocket implements SpaceShip{
     private double weightTonnes = 0;
     private double maxWeightTonnes = 0;
 
-    SpaceShip(double cost, double weight, double maxWeight) {
+    Rocket(double cost, double weight, double maxWeight) {
         this.costMillions = cost;
         this.weightTonnes = weight;
         this.maxWeightTonnes = maxWeight;
 
     }
 
-    @Override
     public boolean launch() {
         return true;
     }
 
-    @Override
     public boolean land() {
         return true;
     }
 
-    @Override
     public boolean canCarry(Item item) {
         if(weightTonnes + item.weight <= maxWeightTonnes){
             return true;
+        } else {
+            return false;
         }
     }
 
-    @Override
     public double carry(Item item) {
-        if(canCarry()) {
+        if (canCarry(item)) {
             this.weightTonnes += item.weight;
-        }
+            return this.weightTonnes;
+        } else {
+            System.out.println("Cargo full, unable to add additional items current cargo weight is: ");}
+            return weightTonnes;
     }
 }
