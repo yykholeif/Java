40ffd208448fc1bc5b7577ff6bf3125d99385004
Youssef Kholeif <yykholeif@gmail.com>
2018-01-03 18:28:20 +0200

    Create functions to measure probability of launch and land failure. In addition, created helper methods (read/write) throughout Rocket and Missile classes

--- ooj/MissionToMars/src/U1.java
+++ ooj/MissionToMars/src/U1.java
@@ -1,10 +1,36 @@
 public class U1 extends Rocket {
 
-    private double costMillions = 100;
-    private double weightTonnes = 10;
-    private double maxWeightTonnes = 18;
+    /**
+     * Rocket prototype =           U$1
+     * Rocket cost =                $100 Million
+     * Rocket weight =              10 Tonnes
+     * Max weight (with cargo) =    18 Tonnes
+     * Chance of launch explosion = 5% * (cargo carried / cargo limit)
+     * Chance of landing crash =    1% * (cargo carried / cargo limit)
+     */
+    private double cargoTonnesCarried = 0;
 
-    U1(double cost, double weight, double maxWeight) {
+    U1(double cost, double weight, double maxWeight, double cargoWeight) {
         super(cost, weight, maxWeight);
+        cargoTonnesCarried = cargoWeight;
     }
+
+    @Override
+    public boolean launch() {
+        double probabilityOfExplosion = 0.05 * (cargoTonnesCarried/getCargoLimit());
+        return getRandomDouble() >= probabilityOfExplosion;
+    }
+
+    @Override
+    public boolean land() {
+        double probabilityOfExplosion = 0.01 * (cargoTonnesCarried/getCargoLimit());
+        return getRandomDouble() >= probabilityOfExplosion;
+    }
+
+    private double getRandomDouble() {
+        double randomNumber = (double) Math.random();
+        return randomNumber;
+    }
+
+
 }
--- ooj/MissionToMars/src/U2.java
+++ ooj/MissionToMars/src/U2.java
@@ -1,10 +1,35 @@
+
 public class U2 extends Rocket {
 
-    private double costMillions = 120;
-    private double weightTonnes = 18;
-    private double maxWeightTonnes = 29;
+    /**
+     * Rocket prototype =           U$2
+     * Rocket cost =                $120 Million
+     * Rocket weight =              18 Tonnes
+     * Max weight (with cargo) =    29 Tonnes
+     * Chance of launch explosion = 4% * (cargo carried / cargo limit)
+     * Chance of landing crash =    8% * (cargo carried / cargo limit)
+     */
+    private double cargoTonnesCarried = 0;
 
-    U2(double cost, double weight, double maxWeight) {
+    U2(double cost, double weight, double maxWeight, double cargoWeight) {
         super(cost, weight, maxWeight);
+        cargoTonnesCarried = cargoWeight;
+    }
+
+    @Override
+    public boolean launch() {
+        double probabilityOfExplosion = 0.04 * (cargoTonnesCarried/getCargoLimit());
+        return getRandomDouble() >= probabilityOfExplosion;
+    }
+
+    @Override
+    public boolean land() {
+        double probabilityOfExplosion = 0.08 * (cargoTonnesCarried/getCargoLimit());
+        return getRandomDouble() >= probabilityOfExplosion;
+    }
+
+    private double getRandomDouble() {
+        double randomNumber = (double) Math.random();
+        return randomNumber;
     }
 }
